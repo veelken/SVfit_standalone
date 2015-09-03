20e74888a1e9aa95c884ed8e5520667a29c3c3a8
Edward Laird <elaird@gmail.com>
2015-09-03 14:25:06 -0400

    add files to trigger build of lcg dict

---
+++ src/classes.h
@@ -0,0 +1,8 @@
+#include "TauAnalysis/SVfitStandalone/interface/SVfitStandaloneLikelihood.h"
+#include "TauAnalysis/SVfitStandalone/interface/SVfitStandaloneAlgorithm.h"
+
+namespace TauAnalysis_SVfitStandalone {
+  struct dictionary1 {
+       SVfitStandaloneAlgorithm svsa;
+  };
+}
